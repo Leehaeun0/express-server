acc1d67da149d49bcca6c38022f4e35673da1c00
ungmo2 <ungmo2@gmail.com>
2019-05-28 22:07:40 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const generateId = () => Math.max(...todos.map(todo => todo.id)) + 1;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send(`<h1>${req.protocol}://${req.get('host')}${req.originalUrl}</h1>`));
+
+app.get('/todos', (req, res) => {
+  console.log('[GET]');
+  res.send(todos);
+});
+
+app.get('/todo/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('[GET] req.params.id => ', req.params.id);
+
+  res.send(todos.filter(todo => todo.id === +id));
+});
+
+app.post('/todo', (req, res) => {
+  const { content } = req.body;
+  console.log('[POST] req.body => ', content);
+
+  todos = [{ id: generateId(), content, completed: false }, ...todos];
+  res.send(todos);
+});
+
+app.delete('/todo/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('[DELETE] req.params.id => ', req.params.id);
+
+  todos = todos.filter(todo => todo.id !== +id);
+  res.send(todos);
+});
+
+app.listen(9000, () => console.log('Simple Rest API Server listening on port 9000'));
