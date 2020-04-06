6ed6540be9c9d8a0c030f3448b066b34ec94bc5a
ungmo2 <ungmo2@gmail.com>
2020-04-06 21:06:29 +0900

    update todos

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(cors());
 
 let todos = [
-  { id: 3, content: 'HTML', completed: false },
+  { id: 3, content: 'Javascript', completed: false },
   { id: 2, content: 'CSS', completed: true },
-  { id: 1, content: 'Javascript', completed: false }
+  { id: 1, content: 'HTML', completed: false }
 ];
 
 app.use(express.static('public'));
